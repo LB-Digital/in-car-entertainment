652d9826d1e36b2d49b61d5bb3dce0cb5fafb628
BowersDev <lucasbowers@me.com>
2020-11-24 23:23:50 +0000

    page transition implemented for Music

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import styled, { useTheme } from 'styled-components';
-import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
 
 
 /* config */
