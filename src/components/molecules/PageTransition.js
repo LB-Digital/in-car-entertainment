331d63a6ed9c0547506511db7453f5faf9ae8a79
BowersDev <lucasbowers@me.com>
2020-11-25 00:52:59 +0000

    page transitions rebuilt entirely and globalised

--- src/components/molecules/PageTransition.js
+++ src/components/molecules/PageTransition.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import styled, { useTheme } from 'styled-components';
-import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
 
 
 /* components */
@@ -9,8 +8,6 @@ import { Header2 } from '../atoms/document_sections';
 
 
 /* styles */
-// config
-
 // styled components
 const StyledPageTransition = styled(
     React.forwardRef((props, ref) => (
@@ -21,13 +18,6 @@ const StyledPageTransition = styled(
   top: 50%;
   transform: translate(-50%, -50%);
   
-  //width: 0;
-  //height: 0;
-  //opacity: 0;
-  //border-radius: 50%;
-  
-  
-  //transition: all 0.2s ease-out;
   
   &.page-transition-enter {
     background: transparent;
@@ -90,79 +80,12 @@ const PageTransitionTitle = styled((props) => <Header2 {...props} />)`
 `;
 
 
-const _PageTransition = ({ navTo, color, title, icon: Icon }) => {
-
-    /* hooks */
-    // styled-components
-    // const theme = useTheme();
-
-    // history
-    // const history = useHistory();
-
-    // refs
-    const pageTransRef = React.createRef();
-
-    // effects
-    // React.useEffect(() => {
-    //     console.log('PageTransition:mount');
-    //
-    //     const transEl = pageTransRef.current;
-    //
-    //     let transInEndCount = 0;
-    //     const onTransitionInEnd = async () => {
-    //         // console.log(`onTransitionInEnd() : ${transInEndCount}`);
-    //
-    //         // only continue once all transitions complete
-    //         // (background, border-radius*4 corners, width, height, opacity)
-    //         if (++transInEndCount !== 8)
-    //             return;
-    //
-    //         // remove event listener
-    //         transEl.removeEventListener('transitionend', onTransitionInEnd);
-    //
-    //         // nav to new page
-    //         history.push(navTo);
-    //
-    //         // wait slight delay
-    //         await new Promise(resolve => setTimeout(resolve, 400));
-    //
-    //         // add new event listener to wait for opacity to fade out
-    //         transEl.addEventListener('transitionend', onTransitionOutEnd, { once: true });
-    //
-    //         // fade away transEl
-    //         transEl.style.opacity = 0;
-    //     };
-    //
-    //     transEl.addEventListener('transitionend', onTransitionInEnd);
-    //
-    //     transEl.style.background = color;
-    //
-    //     const newWidth = theme.screenDimensions.width - (theme.spacing.raw.md * 2);
-    //     const newHeight = theme.screenDimensions.height - (theme.spacing.raw.md * 2);
-    //     transEl.style.width = `${newWidth}px`;
-    //     transEl.style.height = `${newHeight}px`;
-    //     transEl.style.borderRadius = theme.borderRadius;
-    //     transEl.style.opacity = 1;
-    //
-    //
-    //     const onTransitionOutEnd = () => {
-    //         console.log(`onTransitionOutEnd()`);
-    //         onPageTransitionEnd();
-    //     };
-    //
-    //
-    //     return () => {
-    //         console.log('unmount');
-    //         // remove all event listeners in case of unmount midway through transition
-    //         transEl.removeEventListener('transitionend', onTransitionInEnd);
-    //         transEl.removeEventListener('transitionend', onTransitionOutEnd);
-    //     }
-    // });
+const _PageTransition = ({ color, title, icon: Icon }) => {
 
 
     /* render */
     return (
-        <StyledPageTransition ref={pageTransRef} color={color}>
+        <StyledPageTransition color={color}>
             <PageTransitionContent>
                 <Icon size='lg' />
                 <PageTransitionTitle>{ title }</PageTransitionTitle>
